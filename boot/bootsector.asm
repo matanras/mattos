e9da553c22e8661a750b99064ccb464b4a665e83
Matan Rashkovsky <matanras@gmail.com>
2020-03-18 13:01:30 -0700

    Changed bootloader to load kernel by LBA addressing as opposed to CHS.

--- boot/bootsector.asm
+++ boot/bootsector.asm
@@ -1,101 +1,118 @@
 [org 0x7c00]
-	xor ax, ax
-	mov ds, ax
-	mov es, ax
-	cli
-	mov ss, ax
+    mov [boot_drive], dl
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    cli
+    mov ss, ax
     mov sp, 0x8000
     sti
     mov bp, sp ; set the stack safely away from us
-
+    
     push BOOTLOADER_BANNER
     call print_string
     add sp, 2
 
-_disk_load:
-    mov ah, 0x02 ; disk read code
-    mov al, 17 ; read num sectors
-    mov ch, 0x00 ; cylinder 0
-    ; dl <- drive number. Our caller sets it as a parameter and gets it from BIOS
-    ; (0 = floppy, 1 = floppy2, 0x80 = hdd, 0x81 = hdd2)
-    mov dh, 0x00 ; head 0
-    mov cl, 0x02 ; start from sector 2
-    mov bx, KERNEL_OFFSET
+    mov ah, 0x41
+    mov dl, [boot_drive]
+    mov bx, 0x55aa
     int 0x13
-    jc _disk_error ; if error (stored in the carry bit)
+    jc _lba_unsupported
+_disk_load:
+    mov ah, 0x42 ; EDD read
+    mov dl, [boot_drive]
+    mov si, DAP
+    int 0x13
+    jc _disk_error
+    push DISK_SUCCESS
+    call print_string
+    add sp, 2
 
-enter_protected_mode:
-	cli ; disable irqs
-	lgdt [GDT_DESCRIPTOR] ; load gdt
-	mov eax, cr0
-	or eax, 1
-	mov cr0, eax ; turn on protected mode
-	jmp CODE_SEG:_init_pm ; far jump to protected mode.
-						 ; the far jump causes the CPU to flush its cache and pipeline
-						 ; which may consist of invalid real-mode instructions
+_enter_protected_mode:
+    cli ; disable irqs
+    lgdt [GDT_DESCRIPTOR] ; load gdt
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax ; turn on protected mode
+    jmp CODE_SEG:_init_pm ; far jump to protected mode.
+                         ; the far jump causes the CPU to flush its cache and pipeline
+                         ; which may consist of invalid real-mode instructions
+
+_lba_unsupported:
+    push LBA_UNSUPPORTED
+    call print_string
+    add sp, 2
+    jmp $
 _disk_error:
-	push DISK_ERROR
-	call print_string
-	add sp, 2
-	jmp $
+    push DISK_ERROR
+    call print_string
+    add sp, 2
+    jmp $
 
 
 [bits 32]
 _init_pm:
-	mov ax, DATA_SEG
-	mov ds, ax
-	mov es, ax
-	mov ss, ax
-	mov fs, ax
-	mov gs, ax
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov fs, ax
+    mov gs, ax
 
-	mov ebp, 0x9000
-	mov esp, ebp
-
-	call KERNEL_OFFSET
-	jmp $
+    mov ebp, 0x9000
+    mov esp, ebp
+    call KERNEL_OFFSET
+    jmp $
 
 %include "string.asm"
 
+KERNEL_OFFSET equ 0x1000
 
 gdt_start:
 gdt_null:
-	; first segment descriptor is null
-	dd 0x0
-	dd 0x0
+    ; first segment descriptor is null
+    dd 0x0
+    dd 0x0
 
 gdt_code:
-	dw 0xffff ; bits 0:15 of limit
-	dw 0x0 ; bits 0:15 of base
-	db 0x0 ; bits 16:23 of base
-	db 10011010b ; present, ring 0, not trap, code, non-conforming, readable, not accessed
-	db 11001111b ; granularity(1), 32 bit, not 64 bit, no AVL, bits 16:19 of limit
-	db 0x0 ; bits 24:31 of base
+    dw 0xffff ; bits 0:15 of limit
+    dw 0x0 ; bits 0:15 of base
+    db 0x0 ; bits 16:23 of base
+    db 10011010b ; present, ring 0, not trap, code, non-conforming, readable, not accessed
+    db 11001111b ; granularity(1), 32 bit, not 64 bit, no AVL, bits 16:19 of limit
+    db 0x0 ; bits 24:31 of base
 
 gdt_data:
-	dw 0xffff ; bits 0:15 of limit
-	dw 0x0 ; bits 0:15 of base
-	db 0x0 ; bits 16:23 of base
-	db 10010010b ; present, ring 0, not trap, data, no expand-down, writable, not accessed
-	db 11001111b ; granularity(1), 32 bit, not 64 bit, no AVL, bits 16:19 of limit
-	db 0x0 ; bits 24:31 of base
+    dw 0xffff ; bits 0:15 of limit
+    dw 0x0 ; bits 0:15 of base
+    db 0x0 ; bits 16:23 of base
+    db 10010010b ; present, ring 0, not trap, data, no expand-down, writable, not accessed
+    db 11001111b ; granularity(1), 32 bit, not 64 bit, no AVL, bits 16:19 of limit
+    db 0x0 ; bits 24:31 of base
 gdt_end:
 
 GDT_DESCRIPTOR:
-	dw gdt_end - gdt_start - 1 ; always less than 1 of the true size
-	dd gdt_start
+    dw gdt_end - gdt_start - 1 ; always less than 1 of the true size
+    dd gdt_start
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
-KERNEL_OFFSET equ 0x1000
+DAP:
+dap_size: db 0x10
+dap_padding: db 0
+dap_num_sectors: dw 17
+dap_buffer_offset: dw KERNEL_OFFSET
+dap_buffer_segment: dw 0
+dap_lower_lba_dword: dd 1
+dap_upper_lba_dword: dd 0
+
+boot_drive: db 0
 BOOTLOADER_BANNER: db `Mattos bootloader.\r\n`, 0
-DISK_ERROR: db "Disk error", 0
+LBA_UNSUPPORTED: db `LBA addressing mode unsupported.\r\n`, 0
+DISK_ERROR: db `Disk error.\r\n`, 0
+DISK_SUCCESS: db `Read kernel from disk.\r\n`, 0
 
 ; Magic number
 times 510 - ($-$$) db 0
 dw 0xaa55
-
-; boot sector = sector 1 of cyl 0 of head 0 of hdd 0
-; from now on = sector 2 ...
-; dw 0xbbbb ; sector 2 = 512 bytes
